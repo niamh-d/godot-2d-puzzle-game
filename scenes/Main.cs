4c86d0294c088a40f8966f82d8118403b8e37bd5
Niamh <niamh.linguist@gmail.com>
2024-11-24 23:18:21 +0200

    grid snapping

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,16 +1,24 @@
 using Godot;
 using System;
 
+namespace Game;
 public partial class Main : Node2D
+
 {
+	private Sprite2D sprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Main ready");
+		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var mousePos = GetGlobalMousePosition();
+		var gridPos = mousePos / 64;
+		gridPos = gridPos.Floor();
+		sprite.GlobalPosition = gridPos * 64;
 	}
 }
