e682854d1fa0ac2846c30cab0b783a83e2ea3474
Niamh <niamh.linguist@gmail.com>
2024-11-24 23:35:57 +0200

    placeholder building now placed at grid po of left mouse click

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -6,19 +6,44 @@ public partial class Main : Node2D
 
 {
 	private Sprite2D sprite;
+	private PackedScene buildingScene;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 		sprite = GetNode<Sprite2D>("Cursor");
+	}
+
+	public override void _UnhandledInput(InputEvent evt)
+	{
+		if (evt.IsActionPressed("left_click"))
+		{
+			PlaceBuildingAtMousePos();
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var gridPos = GetMouseGridCellPos();
+		sprite.GlobalPosition = gridPos * 64;
+	}
+
+	private Vector2 GetMouseGridCellPos()
+	{
 		var mousePos = GetGlobalMousePosition();
 		var gridPos = mousePos / 64;
 		gridPos = gridPos.Floor();
-		sprite.GlobalPosition = gridPos * 64;
+		return gridPos;
+	}
+
+	private void PlaceBuildingAtMousePos()
+	{
+		var building = buildingScene.Instantiate<Node2D>();
+		AddChild(building);
+
+		var gridPos = GetMouseGridCellPos();
+		building.GlobalPosition = gridPos * 64;
 	}
 }
