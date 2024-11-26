57ef1b1c7f2bf119fdb241c58346959dc0e501f8
Niamh <niamh.linguist@gmail.com>
2024-11-27 01:07:26 +0200

    highlight tiles are shown when placing a building

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -8,13 +8,15 @@ public partial class Main : Node2D
 	private Sprite2D cursor;
 	private PackedScene buildingScene;
 	private Button placeBuildingButton;
+	private TileMapLayer highlightTilemapLayer;
+	private Vector2? hoveredGridCell;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 		cursor = GetNode<Sprite2D>("Cursor");
 		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+		highlightTilemapLayer = GetNode<TileMapLayer>("HighlightTileMapLayer");
 
 		cursor.Visible = false;
 
@@ -30,11 +32,15 @@ public partial class Main : Node2D
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		var gridPos = GetMouseGridCellPos();
 		cursor.GlobalPosition = gridPos * 64;
+		if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPos))
+		{
+			hoveredGridCell = gridPos;
+			UpdateHighlightTileMapLayer();
+		}
 	}
 
 	private Vector2 GetMouseGridCellPos()
@@ -52,6 +58,27 @@ public partial class Main : Node2D
 
 		var gridPos = GetMouseGridCellPos();
 		building.GlobalPosition = gridPos * 64;
+
+		hoveredGridCell = null;
+		UpdateHighlightTileMapLayer();
+	}
+
+	private void UpdateHighlightTileMapLayer()
+	{
+		highlightTilemapLayer.Clear();
+
+		if (!hoveredGridCell.HasValue)
+		{
+			return;
+		}
+
+		for (var x = hoveredGridCell.Value.X - 3; x <= hoveredGridCell.Value.X + 3; x++)
+		{
+			for (var y = hoveredGridCell.Value.Y - 3; y <= hoveredGridCell.Value.Y + 3; y++)
+			{
+				highlightTilemapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
+			}
+		}
 	}
 
 	private void OnButtonPressed()
