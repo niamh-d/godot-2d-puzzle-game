6cfa6d9fc5dc2a203b4d8ba7306afb3f2ff22da6
Niamh <niamh.linguist@gmail.com>
2025-04-04 21:57:07 +0300

    camera panning

--- scenes/level/BaseLevel.cs
+++ scenes/level/BaseLevel.cs
@@ -7,11 +7,17 @@ public partial class BaseLevel : Node
 {
 	private GridManager gridManager;
 	private GoldMine goldMine;
+	private GameCamera gameCamera;
+	private TileMapLayer baseTerrainTilemapLayer;
 
 	public override void _Ready()
 	{
 		gridManager = GetNode<GridManager>("GridManager");
 		goldMine = GetNode<GoldMine>("%GoldMine");
+		gameCamera = GetNode<GameCamera>("GameCamera");
+		baseTerrainTilemapLayer = GetNode<TileMapLayer>("%BaseTerrainTileMapLayer");
+
+		gameCamera.SetBoundingRect(baseTerrainTilemapLayer.GetUsedRect());
 
 		gridManager.GridStateUpdated += OnGridStateUpdated;
 	}
