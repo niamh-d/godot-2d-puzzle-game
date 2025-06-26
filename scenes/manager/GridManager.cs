fa1b22158394a981103bf0926b077ae0b2cb4956
Niamh <niamh.linguist@gmail.com>
2025-06-26 19:08:43 +0300

    fix issue where building cannot be placed on tile with shadow

--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -11,6 +11,7 @@ public partial class GridManager : Node
 {
 	private const string IS_BUILDABLE = "is_buildable";
 	private const string IS_WOOD = "is_wood";
+	private const string IS_IGNORED = "is_ignored";
 
 	[Signal]
 	public delegate void ResourceTilesUpdatedEventHandler(int collectedTiles);
@@ -41,7 +42,7 @@ public partial class GridManager : Node
 		foreach (var layer in allTilemapLayers)
 		{
 			var customData = layer.GetCellTileData(tilePos);
-			if (customData == null) continue;
+			if (customData == null || (bool)customData.GetCustomData(IS_IGNORED)) continue;
 			return (bool)customData.GetCustomData(dataName);
 		}
 		return false;
